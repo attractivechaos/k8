4bf866678455ada40253b4b936a3de0be822c74a
Heng Li <lh3@me.com>
2014-11-08 10:31:13 -0500

    Compute eigenvalues/vectors for symmetric matrices

--- k8.js
+++ k8.js
@@ -341,6 +341,148 @@ Math.m.solve = function(a, b) { // Gauss-Jordan elimination, translated from gau
 	return 0;
 }
 
+Math.m.eigen = function(a)
+{
+	function SQR(a) { return a * a; }
+	function pythag(a, b)
+	{
+		var absa = Math.abs(a), absb = Math.abs(b);
+		if (absa > absb) return absa * Math.sqrt(1.0 + SQR(absb / absa));
+		else return (absb == 0.0 ? 0.0 : absb * Math.sqrt(1.0 + SQR(absa / absb)));
+	}
+	function tred2(a, d, e)
+	{
+		var n = a.length, l, i, j, k; // integers
+		var scale, hh, h, g, f; // real numbers
+
+		for (i = n - 1; i > 0; i--) {
+			l = i - 1;
+			h = scale = 0.0;
+			if (l > 0) {
+				for (k = 0; k < l + 1; k++)
+					scale += Math.abs(a[i][k]);
+				if (scale == 0.0)
+					e[i] = a[i][l];
+				else {
+					for (k = 0; k < l + 1; k++) {
+						a[i][k] /= scale;
+						h += a[i][k] * a[i][k];
+					}
+					f = a[i][l];
+					g = (f >= 0.0 ? -Math.sqrt(h) : Math.sqrt(h));
+					e[i] = scale * g;
+					h -= f * g;
+					a[i][l] = f - g;
+					f = 0.0;
+					for (j = 0; j < l + 1; j++) {
+						/* Next statement can be omitted if eigenvectors not wanted */
+						a[j][i] = a[i][j] / h;
+						g = 0.0;
+						for (k = 0; k < j + 1; k++)
+							g += a[j][k] * a[i][k];
+						for (k = j + 1; k < l + 1; k++)
+							g += a[k][j] * a[i][k];
+						e[j] = g / h;
+						f += e[j] * a[i][j];
+					}
+					hh = f / (h + h);
+					for (j = 0; j < l + 1; j++) {
+						f = a[i][j];
+						e[j] = g = e[j] - hh * f;
+						for (k = 0; k < j + 1; k++)
+							a[j][k] -= (f * e[k] + g * a[i][k]);
+					}
+				}
+			} else
+				e[i] = a[i][l];
+			d[i] = h;
+		}
+		/* Next statement can be omitted if eigenvectors not wanted */
+		d[0] = 0.0;
+		e[0] = 0.0;
+		/* Contents of this loop can be omitted if eigenvectors not wanted except for statement d[i]=a[i][i]; */
+		for (i = 0; i < n; i++) {
+			l = i;
+			if (d[i] != 0.0) {
+				for (j = 0; j < l; j++) {
+					g = 0.0;
+					for (k = 0; k < l; k++)
+						g += a[i][k] * a[k][j];
+					for (k = 0; k < l; k++)
+						a[k][j] -= g * a[k][i];
+				}
+			}
+			d[i] = a[i][i];
+			a[i][i] = 1.0;
+			for (j = 0; j < l; j++)
+				a[j][i] = a[i][j] = 0.0;
+		}
+	}
+	function SIGN(a, b) { return b >= 0.? Math.abs(a) : -Math.abs(a); }
+	function tqli(d, e, z)
+	{
+		var n = d.length, m, l, iter, i, k; // integers
+		var s, r, p, g, f, dd, c, b; // real numbers
+
+		for (i = 1; i < n; i++)
+			e[i - 1] = e[i];
+		e[n - 1] = 0.0;
+		for (l = 0; l < n; l++) {
+			iter = 0;
+			do {
+				for (m = l; m < n - 1; m++) {
+					dd = Math.abs(d[m]) + Math.abs(d[m + 1]);
+					if (Math.abs(e[m]) + dd == dd)
+						break;
+				}
+				if (m != l) {
+					if (iter++ == 30) {
+						warn("[tqli] Too many iterations in tqli.\n");
+						break;
+					}
+					g = (d[l + 1] - d[l]) / (2.0 * e[l]);
+					r = pythag(g, 1.0);
+					g = d[m] - d[l] + e[l] / (g + SIGN(r, g));
+					s = c = 1.0;
+					p = 0.0;
+					for (i = m - 1; i >= l; i--) {
+						f = s * e[i];
+						b = c * e[i];
+						e[i + 1] = (r = pythag(f, g));
+						if (r == 0.0) {
+							d[i + 1] -= p;
+							e[m] = 0.0;
+							break;
+						}
+						s = f / r;
+						c = g / r;
+						g = d[i + 1] - p;
+						r = (d[i] - g) * s + 2.0 * c * b;
+						d[i + 1] = g + (p = s * r);
+						g = c * r - b;
+						/* Next loop can be omitted if eigenvectors not wanted */
+						for (k = 0; k < n; k++) {
+							f = z[k][i + 1];
+							z[k][i + 1] = s * z[k][i] + c * f;
+							z[k][i] = c * z[k][i] - s * f;
+						}
+					}
+					if (r == 0.0 && i >= l)
+						continue;
+					d[l] -= p;
+					e[l] = g;
+					e[m] = 0.0;
+				}
+			} while (m != l);
+		}
+	}
+
+	var ev = [], e = [];
+	tred2(a, ev, e);
+	tqli(ev, e, a);
+	return ev;
+}
+
 /* // Math.m example
 x = [[1,2],[3,4]]; y = [[1,2],[3,4]];
 Math.m.print(Math.m.add(Math.m.mul(x,y), x));
@@ -348,6 +490,16 @@ a = [[1,1],[1,-1]]; b = [[2],[0]];
 a = [[1,2],[3,4]];
 print(Math.m.solve(a));
 Math.m.print(a);
+*/
+/* // eigen value example
+var x = [[10, 1, 2, 3, 4],
+		 [ 1, 9,-1, 2,-3],
+		 [ 2,-1, 7, 3,-5],
+		 [ 3, 2, 3,12,-1],
+		 [ 4,-3,-5,-1,15]];
+var ev = Math.m.eigen(x);
+print(ev.join("\t")); // eigen values
+Math.m.print(x); // eigen vectors
 */
 
 /************************************
